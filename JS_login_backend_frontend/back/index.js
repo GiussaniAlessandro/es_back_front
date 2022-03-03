65b7cee0a89a1227c56e9307aadb66efa8a33cf1
Giussani Alessandro <giussani.alessandro@osdbsesto>
2022-03-03 11:45:07 +0100

    init

--- JS_login_backend_frontend/back/index.js
+++ JS_login_backend_frontend/back/index.js
@@ -5,48 +5,61 @@ apiServer.use(cors());
 var fs = require("fs");
 const { stringify } = require("querystring");
 
+
+
+
+
+var express = require("express");
+var cors = require("cors");
+var apiServer = express();
+apiServer.use(cors());
+var fs = require("fs");
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'giussani.alessandro.tave.osdb.it',
+    user: 'c179_alessandro',
+    password: "Az-61549",
+    database: 'c179_5AI_2021',
+})
+  
 var host = "localhost";
-var port = 3000;
+var port = 5000;
 
-apiServer.listen(port, host, () => {
-  console.log("Server partito: http://%s:%d/", host, port);
-});
+var array = [];
 
-apiServer.get("/api/login", (req, res) => {
-  console.log("ricevuti:", req.query.mail, req.query.password);
-  fs.readFile("users.json", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ message: "errore generico" });
-    } else {
-      var login = false;
-      var users = JSON.parse(data);
-      users.forEach((a) => {
-        if (a.mail === req.query.mail && a.password === req.query.password) {
-          res.status(200).json({ message: "login effettuato" });
-          login = true;
-          return;
+apiServer.listen(port, host, ()=>{
+    console.log("server ---> http://%s:%d", host, port)
+})
+
+apiServer.get("/api/login", (req , res) => {
+    console.log("ricevuti" , req.query.mail , req.query.psw);
+    connection.query(
+        'SELECT * FROM `users` WHERE `mail` = ? AND `psw` = ?',
+        [req.query.mail, req.query.psw],
+        function(err, results, fields) {
+            if (results.length === 0) {
+                res.status(400).json({message: "log non effettuato"});
+            } else {
+                res.status(200).json({message: "log effettuato"});
+            }
         }
-      });
-      if (!login) res.status(400).json({ message: "login failed" });
-    }
-  });
-});
+      );
+})
 
-apiServer.get("/api/register", (req, res) => {
-  console.log("ricevuti:", req.query.mail, req.query.password);
-  fs.readFile("users.json", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ message: "errore generico" });
-    } else {
-      var users = JSON.parse(data);
-      users.push({ mail: req.query.mail, password: req.query.password });
-      fs.writeFile("users.json", JSON.stringify(users), (err) => {
-        if (err) res.status(400).json({ message: "sign-up failed" });
-        else res.status(200).json({ message: "sign-up success" });
-      });
-    }
-  });
-});
+apiServer.get("/api/reg", (req , res) => {
+    console.log("ricevuti" , req.query.mail , req.query.psw);
+    connection.query(
+        "INSERT INTO users (mail, psw) VALUES (?,?);",
+        [req.query.mail, req.query.psw],
+        function(err, results, fields) {
+            console.log(err);
+            if (err) {
+                res.status(400).json({message: "reg non effettuato"});
+            } else {
+                res.status(200).json({message: "reg effettuato"});
+            }
+        }
+      );
+})
 
